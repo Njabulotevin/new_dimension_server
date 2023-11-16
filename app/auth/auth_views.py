a2538ca95728176cbd979abbabd9437ab7cd13fb
WeThinkCode_ <wethinkcode_@pop-os.localdomain>
2023-11-16 17:40:30 +0200

    getting user from database

--- app/auth/auth_views.py
+++ app/auth/auth_views.py
@@ -3,48 +3,45 @@ from utils.response import bad_request, good_request, server_error, not_found, u
 from decouple import config
 from utils.token import gen_token
 from middlewares.auth import protectedMiddleware
+from database.db_conn import get_db_connection
 
 
 
 
 auth_blueprint = Blueprint("admin", __name__, url_prefix='/admin')
 
-
 dummy_users = [
         {
             "id": "a1c28c6b-e932-4423-958a-9ea427e69d0b",
             "username": "user1",
             "email": "user1@example.com",
-            "password": "password1"
+            "password": b"password1"
         },
         {
             "id": "24e7f7a2-72a2-48ab-8776-1f0f3ec4f09b",
             "username": "user2",
             "email": "user2@example.com",
-            "password": "password2"
+            "password": b"password2"
         },
         {
             "id": "c7a301f8-9ca5-4813-920c-798b5e0e3301",
             "username": "user3",
             "email": "user3@example.com",
-            "password": "password3"
+            "password": b"password3"
         },
         {
             "id": "5a9f344a-1a84-43d7-95df-7c6b6e4ddce5",
             "username": "user4",
             "email": "user4@example.com",
-            "password": "password4"
+            "password": b"password4"
         },
         {
             "id": "b9f999f6-8937-48b7-9b8e-0b16dd53c073",
             "username": "user5",
             "email": "user5@example.com",
-            "password": "password5"
+            "password": b"password5"
         }
     ]
-
-
-
 
 @auth_blueprint.get("/<id>")
 @protectedMiddleware
@@ -99,8 +96,10 @@ def is_valid_user(email : str, password : str):
 
 
 def get_user(id = None, email = None) -> dict:
+    conn = get_db_connection()
+    users = conn.
     try:
-        for user in dummy_users:
+        for user in users:
             if user["id"] == id or user["email"] == email:
                 return user
     except:
