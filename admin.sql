a45c12d9a6fbcde21c8427889b65ab7d3ea378ea
WeThinkCode_ <wethinkcode_@pop-os.localdomain>
2024-02-26 10:15:41 +0200

    changed to pipenv

--- admin.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS admin;
-
-CREATE TABLE admin (
-    id UUID PRIMARY KEY,
-    email Char(255) UNIQUE NOT NULL,
-    username CHAR(255) NOT NULL,
-    password CHAR(255) NOT NULL
-);
